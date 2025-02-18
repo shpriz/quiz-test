e8debf409c4e193f837fce48b0740185fd0c9ed0
Shpriz <shpriz1982@gmail.com>
2025-02-18 14:47:20 +0300

    UPD: docker compose nginx  2.3.13.20 dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,23 +6,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: { overlay: false },
-    host: 'stomtest.nsmu.ru',
-    port: 5173,
-    allowedHosts: ['stomtest.nsmu.ru'],
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://stomtest.nsmu.ru:5000',
-        changeOrigin: true,
-        secure: false
-      },
-    },
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
-  }
+    sourcemap: false,
+    minify: true,
+  },
 });
-
